d2c4e22922e28ba90f73c88090c13c9160fd428b
Ester-0 <azalea_@hotmail.com>
2023-10-31 10:57:21 +0100

    Ok

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // ¡Buena suerte!
+const button = document.querySelector("#get-advice-button");
+
+const advice = document.querySelector("#advice");
+const numAdvice = document.querySelector("#advice-id");
+
+async function updateNewAadvice() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+
+    const randomAdvice = await response.json();
+
+    advice.textContent = randomAdvice.slip.advice;
+    numAdvice.textContent = randomAdvice.slip.id;
+
+    console.log("nuevo consejo", randomAdvice);
+
+}
+button.addEventListener("click", updateNewAadvice);
+
+
+
+
+
+
+
